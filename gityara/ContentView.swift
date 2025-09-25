1a3c41381b50f612dc1d22eab02b4379911942da
Mogento <davsmix@gmail.com>
2025-09-25 08:56:56 +0300

    Initial Commit

---
+++ gityara/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  gityara
+//
+//  Created by давид сарангов on 25.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gityara/gityaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  gityaraApp.swift
+//  gityara
+//
+//  Created by давид сарангов on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct gityaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
