d2c311597649a537b72c5dd7077a86fbca14f773
Mogento <davsmix@gmail.com>
2025-09-25 16:39:54 +0300

    add homeView

---
+++ gityara/HomeVIew.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVIew.swift
+//  gityara
+//
+//  Created by давид сарангов on 25.09.2025.
+//
+
+import SwiftUI
+
+struct HomeVIew: View {
+    var body: some View {
+        Text("Screen Two")
+    }
+}
+
+#Preview {
+    HomeVIew()
+}
