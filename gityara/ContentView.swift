42db6411470d3d4e9acca2db0d651a48994cf768
Mogento <davsmix@gmail.com>
2025-09-26 11:41:55 +0300

    add rectangle

--- gityara/ContentView.swift
+++ gityara/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("swift its cool!!")
+            Rectangle()
             
         }
         .padding()
